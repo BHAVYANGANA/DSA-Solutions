741583a31cc34515aeee4e917578c8f73872e079
Bhavyangana Kanthed <68499453+BHAVYANGANA@users.noreply.github.com>
2023-07-02 21:57:52 +0530

    Time: 141 ms (76.38%), Space: 93.3 MB (29.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,22 +4,15 @@ public:
         
        
         int n = prices.size();
-        int i = 0;
-        int j = 1;
+
         int mini = INT_MAX;
         int profit = 0;
-        while(i < n && j <n){
- //           if(j < n && prices[i] < prices[j]){
+        for(int i = 0, j = 1; i,j <n;){
                 mini = min(mini, prices[i]);
                 int cost = prices[j] - mini;
                 profit = max(profit, cost);
-            //     j++;
-
-            // }
-            // else{
                 j++;
                 i++;
-        ////////////   }
         }
 
         return profit;
